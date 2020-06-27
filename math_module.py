241c13bdf36575359aea07f4c075b4e3b8346971
WycliffeMuchumi <muchumimaina94@gmail.com>
2020-06-26 17:25:27 -0700

    random and math modules

---
+++ math_module.py
@@ -0,0 +1,9 @@
+#the math module contains the pi and sin math functions
+ 
+from math import pi, sin
+print('Pi is roughly', pi)
+print('sin(100) =', sin(100))
+
+#Results
+#Pi is roughly 3.141592653589793
+#sin(100) = -0.5063656411097588
---
+++ random_numbers.py
@@ -0,0 +1,13 @@
+#the python program below prints a random number between 1 and 10 by the help of random module
+#it executes the operation by calling the randint function
+#it prints a different number everytime the function is called
+
+from random import randint
+number = randint(1,10)
+print('A random number between 1 and 10:',number)
+
+#Results
+#A random number between 1 and 10: 6
+#A random number between 1 and 10: 7
+#A random number between 1 and 10: 4
+
